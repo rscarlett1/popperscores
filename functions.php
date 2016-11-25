3596eeccd10a5b6472074f141f542285765e6045
rscarlett1 <raewynne.scarlett@gmail.com>
2016-11-26 10:37:50 +1300

    mobile display less font awesome

--- functions.php
+++ functions.php
@@ -119,9 +119,14 @@ function popperscores_scripts() {
 	//Add Google Fonts Open Sans
 	wp_enqueue_style('popperscores-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i');
 
+	wp_enqueue_script('popperscores-fontawesome,'), '<script src="https://use.fontawesome.com/9716eb12cb.js"></script>'
 	
-	wp_enqueue_script( 'popperscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'popperscores-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20120206', true );
 
+	wp_localize_script( 'popperscores-navigation', 'screenReaderText', array(
+		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'Popperscores' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'Popperscores' ) . '</span>',
+	) );
 	wp_enqueue_script( 'popperscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
