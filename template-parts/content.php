6da95dfc73c14fd54eeeebfa7ef51d1b34a2a5e4
rscarlett1 <raewynne.scarlett@gmail.com>
2016-11-27 10:45:26 +1300

    up to continue reading button

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,38 +6,35 @@
  *
  * @package Popperscores
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		
+		<?php the_title( sprintf( '<h2 class="entry-title index-excerpt"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php popperscores_posted_on(); ?>
+		<div class="index-entry-meta">
+			<?php popperscores_index_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content index-excerpt">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'popperscores' ),
-				'after'  => '</div>',
-			) );
+			the_excerpt();
 		?>
 	</div><!-- .entry-content -->
+	
+	<div class="continue-reading">
+		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+			<?php
+				printf(
+					wp_kses( __( 'Continue reading %s', 'popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				);
+			?>
+		</a>
+	</div>
 
-	<footer class="entry-footer">
-		<?php popperscores_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
