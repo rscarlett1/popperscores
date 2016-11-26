2cc561e1acbd94792f965dcdb13362f17460fb34
rscarlett1 <raewynne.scarlett@gmail.com>
2016-11-27 08:23:17 +1300

    comments finished

--- comments.php
+++ comments.php
@@ -9,7 +9,6 @@
  *
  * @package Popperscores
  */
-
 /*
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
@@ -33,19 +32,6 @@ if ( post_password_required() ) {
 				);
 			?>
 		</h2>
-
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'popperscores' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'popperscores' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'popperscores' ) ); ?></div>
-
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
