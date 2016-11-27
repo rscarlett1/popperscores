afe59c680103835d6d01ee9c6414ae65d2db2f12
rscarlett1 <raewynne.scarlett@gmail.com>
2016-11-27 20:48:36 +1300

    header nearly fixed

--- header.php
+++ header.php
@@ -50,7 +50,18 @@
                 <p>My Bag</p>
             </div>
 
-            
+             <div id="background-horse">
+                        <div id="logo-area">
+                           
+                            <?php if ( get_header_image() ) : ?>
+                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+                            </a>
+                            <?php endif;?>
+                            <form>
+                            <input type="text" name="search" placeholder="Search"/>
+                            </form>
+                        </div>            
 
 
 
